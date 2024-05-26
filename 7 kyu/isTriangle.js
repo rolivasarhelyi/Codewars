2b427b2d764aaa35059adf1933ebd9fa4faa7dec
rolivasarhelyi <roli.vasarhelyi@gmail.com>
2024-05-26 21:07:08 +0200

    Add isTriangle.js 7 kyu codewars solution

---
+++ 7 kyu/isTriangle.js
@@ -0,0 +1,4 @@
+//Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
+function isTriangle(a, b, c) {
+  return a + b > c && a + c > b && b + c > a ? true : false;
+}
