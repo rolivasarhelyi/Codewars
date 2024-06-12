6501196bba4e6c9219d61d3d26afbdc4c740ba8d
rolivasarhelyi <roli.vasarhelyi@gmail.com>
2024-06-12 21:06:26 +0200

    Add towerBuilder.js 6 kyu codewars solution

---
+++ 6 kyu/towerBuilder.js
@@ -0,0 +1,11 @@
+function towerBuilder(nFloors) {
+  let tower = [];
+  for (var i = 0; i < nFloors; i++) {
+    tower.push(
+      ' '.repeat(nFloors - i - 1) +
+        '*'.repeat(i * 2 + 1) +
+        ' '.repeat(nFloors - i - 1)
+    );
+  }
+  return tower;
+}
