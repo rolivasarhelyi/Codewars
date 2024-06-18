2c259fa42d7c7198cba8da0e544a6d728a806a40
rolivasarhelyi <roli.vasarhelyi@gmail.com>
2024-06-18 22:30:13 +0200

    Added 6 kyu twoSum.js codewars solution

---
+++ 6 kyu/twoSum.js
@@ -0,0 +1,11 @@
+function twoSum(numbers, target) {
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = 1; j < numbers.length; j++) {
+      if (numbers[i] + numbers[j] === target) {
+        return [i, j];
+      } else {
+        continue;
+      }
+    }
+  }
+}
